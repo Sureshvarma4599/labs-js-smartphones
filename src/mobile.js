952660e342e4e70b06c1d61e6da04b26ae6b65f6
Sureshvarma4599 <sureshvarma.n666@gmail.com>
2020-11-13 09:56:24 +0530

    finalcode

--- src/mobile.js
+++ src/mobile.js
@@ -1,13 +1,102 @@
 //SmartPhone
+class SmartPhone{
+    constructor(ram, batteryPower, price){
+        this.ram=ram
+        this.batteryPower=batteryPower
+        this.price=price
+    }
+   displayFeatures(){
+    var myPhone= new SmartPhone("4GB","3800mAH",24000);
+    return this.ram +'\n'+this.batteryPower+'\n'+this.price;
+   }
+   remainingAmount(priceIHave){
+    this.price = this.price - priceIHave;
+   }
 
+    }
+    
 
 
 // AndroidPhone
+class AndroidPhone extends SmartPhone{
+    constructor(modelName, ram, batteryPower, price) {
+        super(ram,batteryPower,price);
+        this.modelName=modelName
+
+    }
+    displayFeatures(){
+        
+        const myAndroidPhone= new AndroidPhone("onePlus","4GB","3800mAH",24000)
+       // myPhone= new AndroidPhone("oneplus","8GB","5000MaH","45000")
+        return this.modelName +'\n' + this.ram +'\n'+ this.batteryPower +'\n'+ this.price;
+    }
+    remainingAmount(priceIHave){
+        
+       // var priceIHave=500;
+        this.price = this.price - priceIHave;
+        if(this.price>=0){
+            return 'Customer has no more amount';
+        }else {
+            return 'Customer has remaining Rs.'+ this.price +' after buying the android phone';
+        }
+       
+    }
+}
+
+
 
 
 
 // IPhone
 
 
+class IPhone extends SmartPhone{
+    constructor(seriesName, ram, batteryPower, price) {
+        super(ram,batteryPower,price);
+        this.seriesName=seriesName
+       
+    }
+    displayFeatures(){
+        const myIphone= new IPhone("iphone 11","4GB","3800mAH",64000)
+        return this.seriesName +'\n' + this.ram +'\n'+ this.batteryPower +'\n'+ this.price;
+    }
+    remainingAmount(priceIHave){
+         this.price = this.price - priceIHave;
+        if(this.price<=priceIHave){
+            return 'Customer has no more amount';
+        }
+        else if(this.price>priceIHave){
+            return "Customer can't able to buy a phone due to insufficient amount";
+             
+        }else if(this.price<priceIHave){
+           
+            return 'Customer has remaining Rs.'+ this.price +' after buying the android phone';
+        }
+        else {
+           
+        }
+    }
+
+}
 
 //MobileCampus
+class MobileCampus{
+    constructor(NoOfMobiles, NoOfAndroidMobiles, NoOfIPhoneMobiles){
+       this.NoOfMobiles=NoOfMobiles
+       this.NoOfAndroidMobiles=NoOfAndroidMobiles
+       this.NoOfIPhoneMobiles=NoOfIPhoneMobiles
+
+    }
+changeMobileAvailabilityNumber(count,type){
+    if(type === 'android'){
+        this.NoOfAndroidMobiles = this.NoOfAndroidMobiles - count;
+        return 'Available android mobiles are '+this.NoOfAndroidMobiles;
+
+    }
+    if(type === 'iphone'){
+        this.NoOfIPhoneMobiles = this.NoOfIPhoneMobiles - count;
+        return 'Available iphone mobiles are '+this.NoOfIPhoneMobiles;
+    }
+    
+}
+}
